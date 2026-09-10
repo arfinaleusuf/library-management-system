becd9af04c44b5f64e05fdd3e43f756203a5e6be
arfinaleusuf <arfinaleusuf@gmail.com>
2026-09-10 23:36:14 +0600

    add routing and layout components for Home and Login pages

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,9 +1,22 @@
 import { createBrowserRouter } from "react-router";
+import Root from "../layout/Root";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div className="text-red-400">Hello World</div>,
+    element: <Root></Root>,
+    children:[
+        {
+            path: "/",
+            element: <Home/>
+        },
+        {
+            path: "/login",
+            element: <Login/>
+        }
+    ]
   },
 ]);
 
