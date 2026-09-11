ac4327785bf9550a3fd7b2f5428684903292361a
arfinaleusuf <arfinaleusuf@gmail.com>
2026-09-11 23:39:47 +0600

    refactor Login and SignUp components; update input fields and implement signup functionality

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
 import { Link } from "react-router";
 
 
 const Login = () => {
+    
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
         <div className="hero bg-base-200 min-h-screen">
             <div className="hero-content flex-col">
@@ -14,8 +19,8 @@ const Login = () => {
                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                     <div className="card-body">
                         <fieldset className="fieldset">
-                            <label className="label">Email</label>
-                            <input type="email" className="input" placeholder="Email" />
+                            <label className="label">Username</label>
+                            <input type="text" className="input" placeholder="Username" />
                             <label className="label">Password</label>
                             <input type="password" className="input" placeholder="Password" />
                             <div><Link to={"/signup"} className="link link-hover">Don't Have an account?</Link></div>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,26 +1,125 @@
+import { useState } from "react";
 import { Link } from "react-router";
+import { baseurl } from "../services/BaseUrl";
 
 
 const SignUp = () => {
+
+    const [email, setEmail] = useState("");
+    const [username, setUsername] = useState("");
+    const [firstname, setFirstname] = useState("");
+    const [lastname, setLastname] = useState("");
+    const [password, setPassword] = useState("");
+    const [role, setRole] = useState("");
+
+    const handleSignup = async (e) => {
+        e.preventDefault();
+
+        const userData = {
+            email,
+            username,
+            firstname,
+            lastname,
+            password,
+            role
+        };
+
+        const res = await fetch(`${baseurl}/createuser`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(userData)
+        })
+
+        const data = await res.json();
+        console.log(data);
+    };
+
     return (
         <div className="hero bg-base-200 min-h-screen">
             <div className="hero-content flex-col">
                 <div className="text-center">
                     <h1 className="text-5xl font-bold">Sign Up Now!</h1>
                     <p className="py-6 w-96">
-                       Please Fill The Input Currectly
+                        Please Fill The Input Correctly
                     </p>
                 </div>
+
                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                     <div className="card-body">
-                        <fieldset className="fieldset">
-                            <label className="label">Email</label>
-                            <input type="email" className="input" placeholder="Email" />
-                            <label className="label">Password</label>
-                            <input type="password" className="input" placeholder="Password" />
-                            <div><Link to={"/signup"} className="link link-hover">Don't Have an account?</Link></div>
-                            <button className="btn btn-neutral mt-4">Login</button>
-                        </fieldset>
+                        <form onSubmit={handleSignup}>
+                            <fieldset className="fieldset">
+
+                                <label className="label">Email</label>
+                                <input
+                                    type="email"
+                                    className="input"
+                                    placeholder="Email"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+
+                                <label className="label">Username</label>
+                                <input
+                                    type="text"
+                                    className="input"
+                                    placeholder="Username"
+                                    value={username}
+                                    onChange={(e) => setUsername(e.target.value)}
+                                />
+
+                                <label className="label">First Name</label>
+                                <input
+                                    type="text"
+                                    className="input"
+                                    placeholder="First Name"
+                                    value={firstname}
+                                    onChange={(e) => setFirstname(e.target.value)}
+                                />
+
+                                <label className="label">Last Name</label>
+                                <input
+                                    type="text"
+                                    className="input"
+                                    placeholder="Last Name"
+                                    value={lastname}
+                                    onChange={(e) => setLastname(e.target.value)}
+                                />
+
+                                <label className="label">Password</label>
+                                <input
+                                    type="password"
+                                    className="input"
+                                    placeholder="Password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+
+                                <label className="label">Role</label>
+                                <input
+                                    type="text"
+                                    className="input"
+                                    placeholder="librarian or member"
+                                    value={role}
+                                    onChange={(e) => setRole(e.target.value)}
+                                />
+
+                                <div>
+                                    <Link to={"/login"} className="link link-hover">
+                                        Already Have an account?
+                                    </Link>
+                                </div>
+
+                                <button
+                                    type="submit"
+                                    className="btn btn-neutral mt-4"
+                                >
+                                    Sign Up
+                                </button>
+
+                            </fieldset>
+                        </form>
                     </div>
                 </div>
             </div>
